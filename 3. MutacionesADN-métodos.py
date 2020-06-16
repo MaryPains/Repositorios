0ba994de1ce3d04853a38899c22ed3a0a1ad0efc
MARY-PC\Mary <pipunhepera@gmail.com>
2020-06-16 16:42:37 +0200

    Para darle la vuelta a la cadena

--- 3. MutacionesADN-métodos.py
+++ 3. MutacionesADN-métodos.py
@@ -1,2 +1,12 @@
+def crearCadena (cadena):
+    nuevaCadena = []
+    indice = len(cadena)
+    while indice:
+        indice -=1
+        nuevaCadena.append(cadena[indice])
+    return "".join(nuevaCadena)
 
+cadena = "ATGC"
+
+crearCadena(cadena)
 
